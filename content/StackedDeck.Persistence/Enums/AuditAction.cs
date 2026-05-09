5d141e7f54198c9e94ca893ebdb37b1d071e2553
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2026-05-09 19:53:53 +0300

    Add XML docs for AuditAction enumeration members

--- content/StackedDeck.Persistence/Enums/AuditAction.cs
+++ content/StackedDeck.Persistence/Enums/AuditAction.cs
@@ -5,8 +5,23 @@ namespace StackedDeck.Persistence.Template.Enums;
 /// </summary>
 public enum AuditAction
 {
+    /// <summary>
+    /// Represents an insert operation.
+    /// </summary>
     Insert = 1,
+
+    /// <summary>
+    /// Represents an update operation.
+    /// </summary>
     Update,
+
+    /// <summary>
+    /// Represents a delete operation.
+    /// </summary>
     Delete,
+
+    /// <summary>
+    /// Represents a soft delete operation.
+    /// </summary>
     SoftDelete
 }
