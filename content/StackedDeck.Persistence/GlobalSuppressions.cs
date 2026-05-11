30554ff65afc865c0c7fcfee92617fc4bc32b077
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2026-05-11 20:25:36 +0300

    Introduce global suppressions for auto-generated code by EF Core

---
+++ content/StackedDeck.Persistence/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0300:Simplify collection initialization", Justification = "Auto generated code", Scope = "namespaceanddescendants", Target = "~N:StackedDeck.Persistence.Template.Migrations")]
+[assembly: SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Justification = "Auto generated code", Scope = "namespaceanddescendants", Target = "~N:StackedDeck.Persistence.Template;.Migrations")]
+[assembly: SuppressMessage("Roslynator", "RCS1205:Order named arguments according to the order of parameters", Justification = "Auto generated code", Scope = "namespaceanddescendants", Target = "~N:StackedDeck.Persistence.Template;.Migrations")]
